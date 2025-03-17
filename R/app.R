510162faa2cf17210fc42334bdeea3585412fb14
ishikajohar <ishikajohar16@gmail.com>
2025-03-17 21:58:23 +0000

    Updated app.R

--- R/app.R
+++ R/app.R
@@ -1,23 +1,20 @@
-#' Launch the Shiny App
-#'
-#' @export
-run_app <- function() {
-  library(shiny)
+library(shiny)
 
-  # Define UI
-  ui <- fluidPage(
-    titlePanel("Welcome to snply"),
-    sidebarLayout(
-      sidebarPanel("Sidebar content here"),
-      mainPanel("Main panel content here")
-    )
+# Define UI
+ui <- fluidPage(
+  titlePanel("Welcome to snply"),
+  sidebarLayout(
+    sidebarPanel("Sidebar content here"),
+    mainPanel("Main panel content here")
   )
+)
 
-  # Define Server
-  server <- function(input, output, session) {}
-
-  # Return the Shiny app object
-  shinyApp(ui = ui, server = server)
+# Define Server
+server <- function(input, output, session) {
+  # Add server logic here
 }
+
+# Run the Shiny app
+shinyApp(ui = ui, server = server)
 
 
